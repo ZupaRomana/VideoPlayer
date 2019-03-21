a6a11ecda70ea32949c0fc135a2c6da9a6da2f4a
ZupaRomana <adam.romanowski93@gmail.com>
2019-03-21 20:13:02 +0100

    Create mainController and run app

--- js/App.js
+++ js/App.js
@@ -2,5 +2,10 @@
 'use strict';
 
 import MovieService from './shared/movie-service.js';
+import MainController from './controllers/main-controller.js'
 
 const movieService = new MovieService();
+const body = document.getElementsByTagName('body')[0];
+const mainController = new MainController(movieService, body);
+
+mainController.run();
