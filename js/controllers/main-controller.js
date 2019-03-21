085d4073c322df3580a2fa64ef9945163e1997e4
ZupaRomana <adam.romanowski93@gmail.com>
2019-03-21 20:08:26 +0100

    Create MainController class with constructor

---
+++ js/controllers/main-controller.js
@@ -0,0 +1,9 @@
+
+export default class MainController {
+  constructor(service, parentElement) {
+    this.service = service;
+    this.parentElement = parentElement;
+    this.movies = null;
+    this.element = null;
+  }
+}
