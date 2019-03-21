94fd24340667f3279316005b98566ba13ac82d42
ZupaRomana <adam.romanowski93@gmail.com>
2019-03-21 20:11:16 +0100

    Implement run() method. It sets missing fields, renders parent html

--- js/controllers/main-controller.js
+++ js/controllers/main-controller.js
@@ -6,4 +6,21 @@ export default class MainController {
     this.movies = null;
     this.element = null;
   }
+  run() {
+    this.initializeMovies();
+    this.renderHtml();
+    this.initializeElement();
+  }
+
+  initializeMovies() {
+    this.movies = this.service.getMovies();
+  }
+
+  renderHtml() {
+    this.parentElement.innerHTML = `<main id="main"></main>`;
+  }
+
+  initializeElement() {
+    this.element = document.getElementById('main');
+  }
 }
