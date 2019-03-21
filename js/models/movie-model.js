7c175bbcdf53949a2c0e87fa99796d88f71209af
ZupaRomana <adam.romanowski93@gmail.com>
2019-03-21 21:54:01 +0100

    Implement MovieModel class with constructor

--- js/models/movie-model.js
+++ js/models/movie-model.js
@@ -1,4 +1,9 @@
 
 export default class MovieModel {
-
+  constructor({id, url, title, description}) {
+    this.id = id;
+    this.url = url;
+    this.title = title;
+    this.description = description;
+  }
 }
