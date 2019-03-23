7146b212014710601b931571b217ff403fc348c4
ZupaRomana <adam.romanowski93@gmail.com>
2019-03-23 04:00:09 +0100

    Add creating child containing movies list, register event to communicate with it

--- js/controllers/main-controller.js
+++ js/controllers/main-controller.js
@@ -1,10 +1,12 @@
 
 import InputController from './input-controller.js';
+import MoviesListController from './movies-list-controller.js';
 
 export default class MainController {
   constructor(service, parentElement) {
     this.service = service;
     this.parentElement = parentElement;
+    this.moviesListController = null;
     this.movies = null;
     this.element = null;
   }
@@ -12,6 +14,7 @@ export default class MainController {
   run() {
     this.setup();
     this.buildChildren();
+    this.registerEventListeners();
   }
 
   setup() {
@@ -34,10 +37,20 @@ export default class MainController {
 
   buildChildren() {
     this.buildInputController();
+    this.buildMoviesListController();
   }
 
   buildInputController() {
     const inputController = new InputController(this.element);
     inputController.run();
+  }
+
+  buildMoviesListController() {
+    this.moviesListController = new MoviesListController(this.element, this.movies);
+    this.moviesListController.run();
+  }
+
+  registerEventListeners() {
+    this.element.addEventListener('input-change', (e) => this.moviesListController.refresh(e.detail));
   }
 }
